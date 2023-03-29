3acda1fa2d922f955db7292dc2bb357cefb4cb1f
Muhammad Lawal <abbaraees@gmail.com>
2023-03-29 11:17:33 +0100

    Refactor: Rewrite the css classes
    Rewrite the css classes for the login page so that it can target both the login and signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,14 @@
 export default function Login() {
   return (
-    <div className="login--page">
-      <div className="login--container">
+    <div className="auth--page">
+      <div className="auth--container">
         <h1>Login to your Account</h1>
-        <form action="" className="login--form">
+        <form action="" className="auth--form">
           <input type="text" name="username" id="" placeholder="Enter your  username" />
           <input type="password" name="password" id="" placeholder="Enter your password"/>
           <button>Login</button>
         </form>
-        <div className="login--links">
+        <div className="auth--links">
           <a href="#">Create an Account</a>
           <a href="#">Forgot password</a>
         </div>
