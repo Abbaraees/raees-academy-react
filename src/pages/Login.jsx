ded238bb6c7404f0591ea50547ab3318e85d1478
Muhammad Lawal <abbaraees@gmail.com>
2023-10-29 09:17:40 +0100

    feat: Redesing the login and signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,28 @@
+import { NavLink } from "react-router-dom"
+import Navbar from "../components/Navbar"
+
 export default function Login() {
   return (
-    <div className="auth--page">
-      <div className="auth--container">
-        <h1>Login to your Account</h1>
-        <form action="" className="auth--form">
-          <input type="text" name="username" id="" placeholder="Enter your  username" />
-          <input type="password" name="password" id="" placeholder="Enter your password"/>
-          <button>Login</button>
-        </form>
-        <div className="auth--links">
-          <a href="#">Create an Account</a>
-          <a href="#">Forgot password</a>
-        </div>
-      </div>  
-    </div>
+    <>
+      <Navbar />
+      <div className="auth--page">
+          <div className="auth--links">
+            <NavLink to="/signup">Sign Up</NavLink>
+            <NavLink to="/login">Sign In</NavLink>
+          </div>
+        <div className="auth--container">
+          <h1>Sign to your Account</h1>
+          <form action="" className="auth--form">
+            <div className="form-inputs">
+              <input type="text" name="username" id="" placeholder="Enter your  username" />
+              <input type="password" name="password" id="" placeholder="Enter your password"/>
+              <p className="policy-text">By clicking <strong>"Sign in"</strong>, you agree to our Terms of Use and our Privacy Policy.</p>
+            </div>
+            <button>Login</button>
+            <a href="#" className="forgot-password-link">Forgot your password?</a>
+          </form>
+        </div>  
+      </div>
+    </>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,30 @@
+import { NavLink } from "react-router-dom"
+import Navbar from "../components/Navbar"
+
+
 export default function Signup() {
     return (
+      <>
+      <Navbar />
       <div className="auth--page">
+          <div className="auth--links">
+            <NavLink to="/signup">Sign Up</NavLink>
+            <NavLink to="/login">Sign In</NavLink>
+          </div>
         <div className="auth--container">
           <h1>Create New Account</h1>
           <form action="" className="auth--form">
-            <input type="text" name="username" id="" placeholder="Enter your  username" />
-            <input type="email" name="email" id="" placeholder="Enter your  email" />
-            <input type="password" name="password" id="" placeholder="Enter your password"/>
-            <input type="password" name="password2" id="" placeholder="Repeat password"/>
+            <div className="form-inputs">
+              <input type="text" name="username" id="" placeholder="Enter your  username" />
+              <input type="email" name="email" id="" placeholder="Enter your  email" />
+              <input type="password" name="password" id="" placeholder="Enter your password"/>
+              <input type="password" name="password2" id="" placeholder="Repeat password"/>
+              <p className="policy-text">By clicking <strong>"Sign up"</strong>, you agree to our Terms of Use and our Privacy Policy.</p>
+            </div>
             <button>Sign Up</button>
           </form>
-          <div className="auth--links">
-            <a href="#">Create an Account</a>
-            <a href="#">Forgot password</a>
-          </div>
         </div>  
       </div>
+      </>
     )
   }
